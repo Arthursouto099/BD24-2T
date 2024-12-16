77bee92a5af1259a6ae66ca73e40c6ccd02cccb4
NicolasValer <nicolas.p.valer@gmail.com>
2024-12-16 17:19:14 -0300

    up

---
+++ Trabalho/index.sql
@@ -0,0 +1,9 @@
+----  ​murder​ that occurred sometime on ​Jan.15, 2018​ and that it took place in ​SQL City​. 
+-- 20180115, murder, SQL City
+
+--codigo 1 
+select * from crime_scene_report where date = 20180115 and type = 'murder'
+--resposta
+--Security footage shows that there were 2 witnesses. The first witness lives at the last house on "Northwestern Dr". The second witness, named Annabel, lives somewhere on "Franklin Ave".
+
+--codigo 2
